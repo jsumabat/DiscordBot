30f9f343156d9a917a73fbbe09a6209f3c799de0
Jonathan Sumabat <diaxaxx@gmail.com>
2020-04-05 11:18:27 -0400

    Keep coding style consistent

--- src/main/java/Utils/RateLimiter.java
+++ src/main/java/Utils/RateLimiter.java
@@ -7,7 +7,7 @@ public class RateLimiter {
     private static HashMap<Long, Long> normalRateLimitMap = new HashMap<>();
     private static HashMap<Long, Pair<Long, Integer>> exponentialRateLimitMap = new HashMap<>();
 
-    public enum RateLimitLevel{
+    public enum RateLimitLevel {
         /**
          * 1s cooldown for normal commands
          */
@@ -25,33 +25,33 @@ public class RateLimiter {
      * @return returns 0 if the user is not rate limited, else it returns the remaining time limited in milliseconds.
      */
     public static int CheckUserRateLimit(long uid, RateLimitLevel commandLimitLevel) {
-        if (commandLimitLevel == RateLimitLevel.RATE_LIMIT_LEVEL_NORMAL) {
+        if(commandLimitLevel == RateLimitLevel.RATE_LIMIT_LEVEL_NORMAL) {
             if(normalRateLimitMap.containsKey(uid)){
                 long tDiff = System.currentTimeMillis() - normalRateLimitMap.get(uid);
-                if(tDiff <= 1000){
+                if(tDiff <= 1000) {
                     return (int)(1000 - tDiff);
-                }else{
+                } else {
                     normalRateLimitMap.put(uid, System.currentTimeMillis());
                 }
-            }else{
+            } else {
                 normalRateLimitMap.put(uid, System.currentTimeMillis());
             }
         } else {
-            if(exponentialRateLimitMap.containsKey(uid)){
+            if(exponentialRateLimitMap.containsKey(uid)) {
                 long tDiff = System.currentTimeMillis() - exponentialRateLimitMap.get(uid).getFirst();
                 int punishment = exponentialRateLimitMap.get(uid).getSecond();
-                if(tDiff <= Math.pow(2, punishment) * 1000){
-                    if(punishment < 6){
+                if(tDiff <= Math.pow(2, punishment) * 1000) {
+                    if(punishment < 6) {
                         exponentialRateLimitMap.put(uid, new Pair<>(System.currentTimeMillis(), punishment+1));
                         return (int)(Math.pow(2, punishment) * 1000);
-                    }else{
+                    } else {
                         exponentialRateLimitMap.put(uid, new Pair<>(exponentialRateLimitMap.get(uid).getFirst(), punishment));
                         return (int)(Math.pow(2, punishment) * 1000 - tDiff);
                     }
-                }else{
+                } else {
                     exponentialRateLimitMap.put(uid, new Pair<>(System.currentTimeMillis(), 0));
                 }
-            }else{
+            } else {
                 exponentialRateLimitMap.put(uid, new Pair<>(System.currentTimeMillis(), 0));
             }
         }
